28d99f68186b7b7e4a4f131855897cd96c16e419
Lukasz Olczak <lukasz.jan.olczak@gmail.com>
2023-05-21 20:52:01 +0200

    symbol hierarchy

--- src/main/scala/hyps/lang/compiler/semantic/types/Symbol.scala
+++ src/main/scala/hyps/lang/compiler/semantic/types/Symbol.scala
@@ -11,15 +11,21 @@ trait Symbol {
 
 object Symbol {
 
+  trait SymbolType extends Symbol with Type
+
   /** Represents built-in types that are available in the language without compiling definition. */
-  case class BuiltInTypeSymbol(qualifiedName: String) extends Symbol with Type {
+  case class BuiltInTypeSymbol(qualifiedName: String) extends SymbolType {
     override def name: String = qualifiedName
   }
 
-  case class VariableSymbol(name: String, declaration: AST) extends Symbol
-  case class FunctionSymbol(name: String, declaration: AST) extends Symbol
-//  case object ClassSymbol    extends Symbol
-//  case object FunctionSymbol extends Symbol
-//  case object MethodSymbol   extends Symbol
+  trait SymbolReference extends Symbol {
+
+    var dataType: Option[Type] = None
+    //var memoryLocation: Option[MemoryLocation] = None
+
+  }
+
+  case class VariableSymbol(name: String, declaration: AST) extends SymbolReference
+  case class FunctionSymbol(name: String, declaration: AST) extends SymbolReference
 
 }
