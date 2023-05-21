ab20273a90e41e813c2354adb5a455ae2490b3d5
Lukasz Olczak <lukasz.jan.olczak@gmail.com>
2023-05-21 15:10:11 +0200

    imperfect type inference

--- src/main/scala/hyps/lang/compiler/Compiler.scala
+++ src/main/scala/hyps/lang/compiler/Compiler.scala
@@ -1,6 +1,6 @@
 package hyps.lang.compiler
 
-import hyps.lang.compiler.semantic.analysis.SymbolTableConstructionPass
+import hyps.lang.compiler.semantic.analysis.{SymbolTableConstructionPass, TypeInterferencePass}
 import hyps.lang.compiler.syntax.ast.AST
 import hyps.lang.compiler.syntax.parser.PclParser
 
@@ -11,10 +11,21 @@ object Compiler {
     result match {
       case PclParser.Success(program, _) =>
         val boundProgram = program.transformEverywhere(new SymbolTableConstructionPass)
-        boundProgram
+        val typedProgram = inferTypes(boundProgram)
+        typedProgram
       case failure: PclParser.NoSuccess =>
         throw CompilerError(Origin.begin("main"), failure.msg)
     }
+  }
+
+  private def inferTypes(ast: AST): AST = {
+    var footprintBefore = ast.hashCode()
+    var current         = ast
+    do {
+      footprintBefore = current.hashCode()
+      current         = current.transformEverywhere(TypeInterferencePass)
+    } while (footprintBefore != current.hashCode())
+    current
   }
 
 }
--- src/main/scala/hyps/lang/compiler/semantic/analysis/TypeInterferencePass.scala
+++ src/main/scala/hyps/lang/compiler/semantic/analysis/TypeInterferencePass.scala
@@ -1,8 +1,19 @@
 package hyps.lang.compiler.semantic.analysis
 
-import hyps.lang.compiler.syntax.ast.AST
+import hyps.lang.compiler.syntax.ast.Declaration.VariableDeclaration
+import hyps.lang.compiler.syntax.ast.{AST, Typed}
 import hyps.lang.compiler.util.tree.TreeRewriter
 
-class TypeInterferencePass  extends TreeRewriter[AST]{
+/** A transformation that infers the type of untyped constructs.
+  */
+object TypeInterferencePass extends TreeRewriter[AST] {
+
+  rewrite {
+    case varDecl @ VariableDeclaration(name, None, initializer) =>
+      initializer match {
+        case typedExpr: Typed => Typed.TypedVariableDeclaration(name, initializer, typedExpr.`type`)
+        case _                => varDecl
+      }
+  }
 
 }
